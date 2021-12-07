35dfa4344a6149e0ed88de50739a8182b1cc4cb2
LPbigFish <82438197+LPbigFish@users.noreply.github.com>
2021-12-07 11:23:21 +0100

    Update

---
+++ Pr_2C_3334/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace Pr_2C_3334
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MineBT = new System.Windows.Forms.Button();
+            this.BlockIdTB = new System.Windows.Forms.TextBox();
+            this.DataTB = new System.Windows.Forms.RichTextBox();
+            this.PrevBlockTB = new System.Windows.Forms.TextBox();
+            this.NonceTB = new System.Windows.Forms.TextBox();
+            this.ThisBlockTB = new System.Windows.Forms.TextBox();
+            this.NextBT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MineBT
+            // 
+            this.MineBT.Location = new System.Drawing.Point(12, 547);
+            this.MineBT.Name = "MineBT";
+            this.MineBT.Size = new System.Drawing.Size(75, 23);
+            this.MineBT.TabIndex = 0;
+            this.MineBT.Text = "Mine";
+            this.MineBT.UseVisualStyleBackColor = true;
+            this.MineBT.Click += new System.EventHandler(this.MineBT_Click);
+            // 
+            // BlockIdTB
+            // 
+            this.BlockIdTB.Location = new System.Drawing.Point(12, 14);
+            this.BlockIdTB.Name = "BlockIdTB";
+            this.BlockIdTB.ReadOnly = true;
+            this.BlockIdTB.Size = new System.Drawing.Size(775, 20);
+            this.BlockIdTB.TabIndex = 1;
+            // 
+            // DataTB
+            // 
+            this.DataTB.Location = new System.Drawing.Point(12, 40);
+            this.DataTB.Name = "DataTB";
+            this.DataTB.Size = new System.Drawing.Size(775, 423);
+            this.DataTB.TabIndex = 2;
+            this.DataTB.Text = "";
+            // 
+            // PrevBlockTB
+            // 
+            this.PrevBlockTB.Location = new System.Drawing.Point(12, 495);
+            this.PrevBlockTB.Name = "PrevBlockTB";
+            this.PrevBlockTB.ReadOnly = true;
+            this.PrevBlockTB.Size = new System.Drawing.Size(775, 20);
+            this.PrevBlockTB.TabIndex = 3;
+            // 
+            // NonceTB
+            // 
+            this.NonceTB.Location = new System.Drawing.Point(12, 469);
+            this.NonceTB.Name = "NonceTB";
+            this.NonceTB.ReadOnly = true;
+            this.NonceTB.Size = new System.Drawing.Size(775, 20);
+            this.NonceTB.TabIndex = 4;
+            // 
+            // ThisBlockTB
+            // 
+            this.ThisBlockTB.Location = new System.Drawing.Point(12, 521);
+            this.ThisBlockTB.Name = "ThisBlockTB";
+            this.ThisBlockTB.ReadOnly = true;
+            this.ThisBlockTB.Size = new System.Drawing.Size(775, 20);
+            this.ThisBlockTB.TabIndex = 5;
+            // 
+            // NextBT
+            // 
+            this.NextBT.Location = new System.Drawing.Point(713, 547);
+            this.NextBT.Name = "NextBT";
+            this.NextBT.Size = new System.Drawing.Size(75, 23);
+            this.NextBT.TabIndex = 6;
+            this.NextBT.Text = "Next Block";
+            this.NextBT.UseVisualStyleBackColor = true;
+            this.NextBT.Click += new System.EventHandler(this.NextBT_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 582);
+            this.Controls.Add(this.NextBT);
+            this.Controls.Add(this.ThisBlockTB);
+            this.Controls.Add(this.NonceTB);
+            this.Controls.Add(this.PrevBlockTB);
+            this.Controls.Add(this.DataTB);
+            this.Controls.Add(this.BlockIdTB);
+            this.Controls.Add(this.MineBT);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button MineBT;
+        private System.Windows.Forms.TextBox BlockIdTB;
+        private System.Windows.Forms.RichTextBox DataTB;
+        private System.Windows.Forms.TextBox PrevBlockTB;
+        private System.Windows.Forms.TextBox NonceTB;
+        private System.Windows.Forms.TextBox ThisBlockTB;
+        private System.Windows.Forms.Button NextBT;
+    }
+}
+
---
+++ Pr_2C_3334/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pr_2C_3334
+{
+    public partial class Form1 : Form
+    {
+        bool zalomeno = false;
+        public int BlockID = 0;
+        public string Data, Nonce, PrevBlock = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", ThisBlock;
+        readonly List<Block> BlockChain = new List<Block>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BlockIdTB.Text = BlockID.ToString();
+            PrevBlockTB.Text = PrevBlock;
+        }
+
+        private void NextBT_Click(object sender, EventArgs e)
+        {
+            BlockID += 1; BlockIdTB.Text = BlockID.ToString();
+            PrevBlockTB.Text = ThisBlock;
+            ThisBlockTB.Text = "";
+            NonceTB.Text = "";
+            DataTB.Text = "";
+            zalomeno = false;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MineBT_Click(object sender, EventArgs e)
+        {
+            if (zalomeno == false)
+            {
+                Data = DataTB.Text;
+                ThisBlock = "0";
+                int j = 0;
+                string i = "000000000";
+                while (!ThisBlock.StartsWith("00000"))
+                {
+                    Nonce = Zalomení.Generatinon(i);
+                    ThisBlock = SHA512((BlockID + Data, Nonce + PrevBlock).ToString());
+                    j++;
+                    i = $"{j:000000000}";
+                }
+
+                ThisBlockTB.Text = ThisBlock;
+                NonceTB.Text = Nonce;
+                BlockChain.Add(new Block(BlockID, Data, Nonce, PrevBlock, ThisBlock));
+                MessageBox.Show("Block byl zalomen!");
+                zalomeno = true;
+            }
+            else
+            {
+                MessageBox.Show("Nemůžete edditovat už zalomený BLOCK!" + Environment.NewLine + "Klikněte na tlačítko next pro vygenerování nového BLOCKU.");
+            }
+            
+        }
+
+        public static string SHA512(string input)
+        {
+            var bytes = System.Text.Encoding.UTF8.GetBytes(input);
+            using (var hash = System.Security.Cryptography.SHA512.Create())
+            {
+                var hashedInputBytes = hash.ComputeHash(bytes);
+                var hashedInputStringBuilder = new System.Text.StringBuilder(128);
+                foreach (var b in hashedInputBytes)
+                    hashedInputStringBuilder.Append(b.ToString("X2"));
+                return hashedInputStringBuilder.ToString();
+            }
+        }
+    }
+
+    public static class Zalomení
+    {
+        static readonly char[] Match = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j' ,'k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','C','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z'};
+
+        public static string Generatinon(string next)
+        {
+            string result = "";
+
+            foreach (char i in next)
+            {
+                string j = i.ToString();
+                result += Match[Convert.ToInt32(j)];   
+            }
+
+            return result;
+        }
+    }
+
+    public class Block
+    {
+        public int BlockID;
+        public string Data, Nonce, PrevBlock, ThisBlock;
+        public Block(int _BlockID, string _Data, string _Nonce, string _PrevBlock, string _ThisBlock)
+        {
+            BlockID = _BlockID;
+            Data = _Data;
+            Nonce = _Nonce;
+            PrevBlock = _PrevBlock;
+            ThisBlock = _ThisBlock;
+        }
+    }
+}
